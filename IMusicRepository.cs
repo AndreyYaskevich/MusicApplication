1fec7d0a2e100112b1938b9acc19a9e304fe6a1b
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-09-11 14:11:43 +0300

    no message

---
+++ IMusicRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query;
+using MusicApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MusicApplication
+{
+    public interface IMusicRepository<T> where T: class
+    {
+        /*
+        
+        IEnumerable<Album> GetAlbums();
+        Album GetAlbumById(int albumId);
+        void Insert(Album album);
+        void Delete(int id);
+        void Update(Album album);
+        void Save();
+        */
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+
+        
+        T GetById(int id);
+        void Insert(T entity);
+        void Delete(int id);
+        void Update(T entity);
+        void Save();
+        Song GetLast();
+    }
+}
---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApplication.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Album>().HasData(
+                new Album
+                {
+                    Id = 1,
+                    Name = "Album1"
+                    
+                    
+                },
+                new Album 
+                {
+                    Id = 2,
+                    Name = "Album2"
+                });
+            builder.Entity<Song>().HasData(
+                new Song
+                {
+                    Id = 1,
+                    AlbumId = 1,
+                    Name = "Song1",
+                    Author="Author1",
+                    
+                },
+                new Song
+                {
+                    Id = 2,
+                    AlbumId = 2,
+                    Name = "Song2",
+                    Author = "Author2"
+                },
+                new Song
+                {
+                    Id = 3,
+                    AlbumId = 1,
+                    Name = "Song3",
+                    Author = "Author3"
+                });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=musicstoredb; Trusted_Connection=True;");
+        }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+       
+    }
+}
