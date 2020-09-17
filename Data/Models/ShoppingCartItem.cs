d36f9cf78a916d2c7be11a5f1e6da22099a3c408
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-09-17 10:32:13 +0300

    Added ShoppingCartService

---
+++ Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,18 @@
+﻿using MusicApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApplication.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        public string ItemId { get; set; }
+        public Album Album { get; set; }
+        public int AlbumId { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCartId { get; set; }
+
+    }
+}
---
+++ Services/ShoppingCartService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using MusicApplication.Data.Models;
+using MusicApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApplication.Services
+{
+    public class ShoppingCartService : IDisposable
+    {
+        private ApplicationContext _context;
+        private ShoppingCartService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public static ShoppingCartService GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+
+            var context = services.GetService<ApplicationContext>();
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCartService(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Album album, int amount)
+        {
+            _context.ShoppingCartItems.Add(
+                new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Album = album,
+                    Amount = amount
+                });
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingCartItem> GetCartItems()
+        {
+            return _context.ShoppingCartItems ?? 
+        }
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+                _context = null;
+            }
+        }
+    }
+}
